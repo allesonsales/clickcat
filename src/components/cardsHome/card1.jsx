2a6df9b40089f30016777d5e69fabfa66279a477
allesonsales <107868418+allesonsales@users.noreply.github.com>
2025-02-12 20:37:18 -0300

    Update card1.jsx

--- src/components/cardsHome/card1.jsx
+++ src/components/cardsHome/card1.jsx
@@ -41,7 +41,7 @@ const Card1 = ({ nome }) => {
                     </div>
                 </div>
                 <div className="cardImage">
-                    <img src="/cat1.svg" alt="" />
+                    <img src="/clickcat/cat1.svg" alt="" />
                 </div>
                 <div className="cardNext" onClick={toggle}>
                     <i class="bi bi-arrow-right-circle-fill"></i>
